f7f0d883a7d843843fb1d3dc86d178781c7eb786
Gabrielli Venerando <107216833+gabivenerando@users.noreply.github.com>
2022-08-18 16:31:04 -0300

    Revert "Sp1 792"

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/ports/repositories/MusicRepositoryPort.java
+++
@@ -1,9 +0,0 @@
-package com.ciandt.summit.bootcamp2022.domain.ports.repositories;
-
-import com.ciandt.summit.bootcamp2022.domain.Music;
-
-import java.util.Set;
-
-public interface MusicRepositoryPort {
-    Set<Music> getMusicsByFilter(); //-> provisory
-}
